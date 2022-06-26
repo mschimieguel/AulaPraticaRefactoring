9ac79ac3eecf138e41179c8dca824d3a193abcc2
mschimieguel <matheussilva@dcc.ufmg.br>
2022-06-26 16:17:24 -0300

    Commit 11

--- Movie.java
+++ Movie.java
@@ -3,52 +3,64 @@ public class Movie {
     public static final int  CHILDRENS = 2;
     public static final int  REGULAR = 0;
     public static final int  NEW_RELEASE = 1;
-  
+
+    private Price _price;
     private String _title;
-    private int _priceCode;
-  
-    public Movie(String title, int priceCode) {
-        _title = title;
-        _priceCode = priceCode;
+
+    public Movie(String name, int priceCode) {
+        _title = name;
+        setPriceCode(priceCode);
     }
-  
+
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
-  
+   
     public void setPriceCode(int arg) {
-        _priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
-  
-    public String getTitle (){
-        return _title;
-    }
-  
-      public double getCharge(int daysRented){
-          double thisAmount = 0;
-          switch (this.getPriceCode()) {
-          case Movie.REGULAR:
-              thisAmount += 2;
-              if (daysRented > 2)
-                  thisAmount += (daysRented - 2) * 1.5;
-              break;
-          case Movie.NEW_RELEASE:
-              thisAmount += daysRented * 3;
-              break;
-          case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (daysRented > 3)
-                  thisAmount += (daysRented - 3) * 1.5;
-              break;
-          }
-          return thisAmount;
-      }
-  
-      public int getFrequentRenterPoints(int daysRented){
-          if ((this.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-              return 2;
-  
-          return 1;
-     }
-  
+
+  public String getTitle (){
+      return _title;
   }
+
+    public double getCharge(int daysRented){
+        double thisAmount = 0;
+        switch (this.getPriceCode()) {
+        case Movie.REGULAR:
+            thisAmount += 2;
+            if (daysRented > 2)
+                thisAmount += (daysRented - 2) * 1.5;
+            break;
+        case Movie.NEW_RELEASE:
+            thisAmount += daysRented * 3;
+            break;
+        case Movie.CHILDRENS:
+            thisAmount += 1.5;
+            if (daysRented > 3)
+                thisAmount += (daysRented - 3) * 1.5;
+            break;
+        }
+        return thisAmount;
+    }
+
+    public int getFrequentRenterPoints(int daysRented){
+        if ((this.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+            return 2;
+
+        return 1;
+   }
+
+}
