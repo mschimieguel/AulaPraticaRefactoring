614ab002d77b3232567718f1fa5bf1d8945d7815
mschimieguel <matheussilva@dcc.ufmg.br>
2022-06-26 14:07:58 -0300

    Commit 5

--- Rental.java
+++ Rental.java
@@ -15,4 +15,23 @@ public class Rental {
    public Movie getMovie() {
       return _movie;
    }
+   public Double getCharge() {
+      double thisAmount = 0; 
+      switch (this.getMovie().getPriceCode()) {
+         case Movie.REGULAR:
+         thisAmount += 2;
+            if (this.getDaysRented() > 2)
+              return thisAmount += (this.getDaysRented() - 2) * 1.5;
+            
+         case Movie.NEW_RELEASE:
+         return thisAmount += this.getDaysRented() * 3;
+            
+         case Movie.CHILDRENS:
+            thisAmount += 1.5;
+            if (this.getDaysRented() > 3)
+            return thisAmount += (this.getDaysRented() - 3) * 1.5;
+         default:
+            throw new java.lang.Error("this is very bad");
+      }
+   }
 }
